95c830d19774f5dfd2e051ff2c2a66ed6675811b
João Ferreira <joao.ferreira@corp.globo.com>
2016-08-15 17:39:38 -0300

    Novo exemplo

--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import "fmt"
 
-func inc(x int) {
-	x++
+func inc(x *int) {
+	*x++
 	fmt.Println(x, &x) // &x = endereco de memoria
 }
 
 func main() {
 	n := 10
 	fmt.Println("antes", n, &n)
-	inc(n)
+	inc(&n)
 	fmt.Println("depois", n, &n)
 }
