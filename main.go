8776da91cd78c8f35fa64898118d03c6c60751ac
João Ferreira <joao.ferreira@corp.globo.com>
2016-08-15 19:02:55 -0300

    Novo exemplo

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 import "fmt"
 
+func exec(cmd string, args ...string) {
+	fmt.Println(cmd, args)
+}
+
 func main() {
-	numeros := []int{1, 2, 3, 4, 5}
-	outrosNumeros := []int{10, 11, 12}
-
-	numeros = append(numeros, outrosNumeros...)
-
-	fmt.Println(numeros)
+	exec("ps", "-e", "-f")
 }
