85fd49c23d1af3b2d81d4c725d10398f010c2522
João Ferreira <joao.ferreira@corp.globo.com>
2016-08-15 19:21:57 -0300

    Novo exemplo

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import "fmt"
 
+type user struct {
+	name string
+	age int
+}
+
 func main() {
-	nomeIdade := map[string]int{}
-	nomeIdade["joao"] = 27
-	nomeIdade["lorena"] = 27
-	nomeIdade["babi"] = 1
+	nomeIdade := map[string]user{}
+	nomeIdade["joao"] = {name: "joao", idade:27}
+	nomeIdade["lorena"] = {name: "lorena", idade:27}
+	nomeIdade["babi"] = {name: "babi", idade:1}
 
 	for nome, idade := range nomeIdade {
 		fmt.Println(nome, idade)
