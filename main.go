de704123c1355d55c738e9731dfd52083fb6280a
João Ferreira <joao.ferreira@corp.globo.com>
2016-08-15 18:54:30 -0300

    Novo exemplo

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	num := make([]int, 2, 10)
-	fmt.Println(num, len(num), cap(num))
-	num = append(num, 1, 2, 3, 4)
-	fmt.Println(num, len(num), cap(num))
-	num = append(num, 1, 2, 3, 4, 5)
-	fmt.Println(num, len(num), cap(num))
+	num := []int{1, 2, 3, 4, 5}
+
+	for i, n := range num {
+		fmt.Println(i, n)
+	}
+
+	for i := 0; i < len(num); i++ {
+		fmt.Println(i, num[i])
+	}
 }
