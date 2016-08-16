2a7fe89be8ad36b639a883ff033abf9361463b94
João Ferreira <joao.ferreira@corp.globo.com>
2016-08-16 17:28:18 -0300

    Novo exemplo

--- main.go
+++ main.go
@@ -2,50 +2,15 @@ package main
 
 import "fmt"
 
-type Human struct{}
-type Cat struct{}
-
-func (Human) Speak() {
-	fmt.Println("Oi!")
+type user struct {
+	name string
 }
 
-func (Human) Walk() {
-	fmt.Println("going")
-}
-
-func (Cat) Speak() {
-	fmt.Println("miau!")
-}
-
-type Speaker interface {
-	Speak()
-}
-
-type Walker interface {
-	Walk()
-}
-
-type WalkerAndSpeaker interface {
-	Walker
-	Speaker
-}
-
-func speak(speaker Speaker) {
-	speaker.Speak()
-}
-
-func walk(w Walker) {
-	w.Walk()
-}
-
-func walkAndSpeak(w WalkerAndSpeaker) {
-	w.Walk()
-	w.Speak()
+func authenticate(u user) bool {
+	return u.name == "joao"
 }
 
 func main() {
-	h := Human{}
-	c := Cat{}
-	walkAndSpeak(h)
-	speak(c)
+	u := user{"joao"}
+	fmt.Println(authenticate(u))
 }
