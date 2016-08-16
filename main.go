e6691081b5d6a9add557b3c411813b69655f409f
João Ferreira <joao.ferreira@corp.globo.com>
2016-08-16 19:39:25 -0300

    Novo exemplo

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func writeCities() {
 	cities["curitiba"] = 41
 	cities["rio"] = 21
 	cities["imperatriz"] = 99
+	fmt.Println(cities)
 }
 
 func main() {
@@ -25,7 +26,7 @@ func main() {
 	defer m.Unlock()
 	m.Lock()
 	cities["sp"] = 11
-	time.Sleep(10 * time.Second)
-
+	time.Sleep(1)
 	fmt.Println(cities)
+	time.Sleep(60 * time.Second)
 }
