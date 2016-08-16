10407da20b0be0c9e0f86709f93864d6eb3f5c77
João Ferreira <joao.ferreira@corp.globo.com>
2016-08-16 19:12:49 -0300

    Novo exemplo

--- main.go
+++ main.go
@@ -1,22 +1,17 @@
 package main
 
-import "fmt"
-
-func fibo(n int, c chan int) {
-	x, y := 0, 1
-	for i := 0; i < n; i++ {
-		x, y = y, x+y
-		c <- x
-	}
-	close(c)
-}
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	c := make(chan int)
 
-	go fibo(10, c)
-
-	for n := range c {
-		fmt.Println(n)
+	select {
+	case m := <-c:
+		fmt.Println(m)
+	case <-time.After(5 * time.Second):
+		fmt.Println("Timeout")
 	}
 }
